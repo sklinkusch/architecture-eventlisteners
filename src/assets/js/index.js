155227890e1c9c1b3f6ae33e8aba0641683de96e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-03-18 10:20:07 +0100

    query form in js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,10 @@
-import "@scss/styles.scss"
+import "@scss/styles.scss";
+
+// Helper
+const $ = selector => document.querySelector(selector);
+
+const addNoteInput = $("#add-note");
+const addNoteButton = $("#add-note-button");
+
+console.log(addNoteInput);
+console.log(addNoteButton);
